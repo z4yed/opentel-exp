d0f9c6e4ccdc5a55d0cae72df85d0cb22c877111
Zayed Hassan <54860522+z4yed@users.noreply.github.com>
2024-11-05 12:37:57 +0600

    opentel configured

--- media-uploader/app.js
+++ media-uploader/app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const mediaRoutes = require("./routes/mediaRoutes");
+const configureOpenTelemetry = require("./opentelemetry");
+
 require("dotenv").config();
 
 const app = express();
 const PORT = 3001;
+
+configureOpenTelemetry("media-uploader-service");
+
+// app.use("/upload", (req, res, next) => {
+//   // console.log("upload endpoint middleware");
+//   // console.log("traceProvider", traceProvider);
+//   // const tracer = traceProvider.getTracer("media-uploader-tracer");
+//   // const span = tracer.startSpan("upload-endpoint");
+
+//   const tracer = opentelemetry.trace.getTracer("media-uploader-tracer", "1.0");
+
+//   span.setAttribute("raw-endpoint", req.originalUrl);
+
+//   context.with(trace.setSpan(context.active(), span), () => {
+//     next();
+//   });
+// });
 
 mongoose.connect("mongodb://mongo:27017/opentel");
 app.set("view engine", "ejs");
--- media-uploader/controllers/mediaController.js
+++ media-uploader/controllers/mediaController.js
@@ -3,9 +3,14 @@ const axios = require("axios");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const MediaModel = require("../models/mediaModel");
 const { MEDIA_CONVERT_SERVICE_URL } = require("../constants");
+const opentelemetry = require("@opentelemetry/api");
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
+const mediaUploadTracer = opentelemetry.trace.getTracer(
+  "media-uploader-tracer",
+  "1.0"
+); // start a tracer
 
 const renderIndex = (req, res) => {
   res.render("index", {
@@ -15,39 +20,57 @@ const renderIndex = (req, res) => {
 };
 
 const uploadFile = async (req, res) => {
-  const file = req.file;
-  const s3 = new S3Client({ region: process.env.AWS_REGION });
-  const input = {
-    Bucket: process.env.S3_BUCKET_NAME,
-    Key: file.originalname,
-    Body: file.buffer,
-    ContentType: file.mimetype,
-  };
+  return mediaUploadTracer.startActiveSpan("uploadFile", async (span) => {
+    try {
+      span.setAttribute("raw-endpoint", "req.original");
+      span.setAttribute("file.size", req.file.size);
+      span.setAttribute("file.type", req.file.mimetype);
 
-  const putCommand = new PutObjectCommand(input);
-  const response = await s3.send(putCommand);
+      const file = req.file;
+      const s3 = new S3Client({ region: process.env.AWS_REGION });
+      const input = {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: file.originalname,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      };
 
-  if (response.$metadata.httpStatusCode === 200) {
-    const media = new MediaModel({
-      filename: file.originalname,
-      filepath: `${process.env.CLOUDFRONT_URL}/${file.originalname}`,
-      mimetype: file.mimetype,
-    });
+      const putCommand = new PutObjectCommand(input);
+      const response = await s3.send(putCommand);
 
-    await media.save();
+      if (response.$metadata.httpStatusCode === 200) {
+        const media = new MediaModel({
+          filename: file.originalname,
+          filepath: `${process.env.CLOUDFRONT_URL}/${file.originalname}`,
+          mimetype: file.mimetype,
+        });
 
-    const convertResponse = await axios.post(
-      `${MEDIA_CONVERT_SERVICE_URL}/convert`,
-      {
-        bucket: process.env.S3_BUCKET_NAME,
-        key: file.originalname,
+        await media.save();
+
+        // pass context to the next service
+        const convertResponse = await axios.post(
+          `${MEDIA_CONVERT_SERVICE_URL}/convert`,
+          {
+            bucket: process.env.S3_BUCKET_NAME,
+            key: file.originalname,
+          }
+        );
+
+        console.log("Media converted", convertResponse.data);
       }
-    );
 
-    console.log("Media converted", convertResponse.data);
-  }
-
-  res.status(200).send("File uploaded to S3");
+      res.status(200).send("File uploaded to S3");
+    } catch (error) {
+      console.error(error);
+      span.setStatus({
+        code: opentelemetry.SpanStatusCode.ERROR,
+        message: error.message,
+      });
+    } finally {
+      console.log("Span end");
+      span.end();
+    }
+  });
 };
 
 module.exports = {
